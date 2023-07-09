01675987e96772d8486ae520bde7d0b002a0cfc0
Casey McCormack <caseymccormack858@gmail.com>
2023-07-08 23:54:01 -0700

    sword function

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(src, positionLeft, positionBottom) {
+    let image = document.createElement('img');
+    image.src = src;
+    image.style.position = 'fixed';
+    image.style.left = positionLeft;
+    image.style.bottom = positionBottom;
+    document.body.append(image);
+  }
+  
+  newImage('assets/green-character.gif', '100px', '100px');
+  newImage('assets/pine-tree.png', '450px', '200px');
+  newImage('assets/tree.png', '200px', '300px');
+  newImage('assets/pillar.png', '350px', '100px');
+  newImage('assets/crate.png', '150px', '200px');
+  newImage('assets/well.png', '500px', '425px');
+
+  function newItems(src, positionLeft, positionBottom) {
+    let sword = document.createElement('img')
+    sword.src = 'assets/sword.png'
+    sword.position = 'fixed'
+    sword.left = '500px'
+    sword.bottom = '405px'
+    document.body.append(sword)
+    
+    sword.addEventListener('click', function(){
+        sword.remove()
+    })
+    
+
+  }
