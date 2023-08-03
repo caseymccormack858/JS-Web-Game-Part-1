e3de520cc196d0adf1c6bb5576fd9d7263f7f233
Casey McCormack <caseymccormack858@gmail.com>
2023-08-03 13:09:13 -0700

    added object added new item function

--- index.js
+++ index.js
@@ -1,34 +1,28 @@
-function newImage(src, positionLeft, positionBottom) {
-    let image = document.createElement('img');
-    image.src = src;
-    image.style.position = 'fixed';
-    image.style.left = positionLeft;
-    image.style.bottom = positionBottom;
-    document.body.append(image);
-  }
-  
-  newImage('assets/green-character.gif', '100px', '100px');
-  newImage('assets/pine-tree.png', '450px', '200px');
-  newImage('assets/tree.png', '200px', '300px');
-  newImage('assets/pillar.png', '350px', '100px');
-  newImage('assets/crate.png', '150px', '200px');
-  newImage('assets/well.png', '500px', '425px');
+function newImage(url, left, bottom){
+  let object = document.createElement('img')
+  object.src = url
+  object.style.position = 'fixed'
+  object.style.left = left + 'px'
+  object.style.bottom = bottom + 'px'
+  document.body.append(object)
+  return object
+}
 
-  function newItem(src, positionLeft, positionBottom) {
-    let sword = document.createElement('img')
-    sword.src = 'assets/sword.png'
-    sword.position = 'fixed'
-    sword.left = '500px'
-    sword.bottom = '405px'
-    document.body.append(sword)
-    
-    sword.addEventListener('dblclick', function(){
-        sword.remove()
-    })
-    
+function newItem(url, left, bottom){
+  let object = newImage(url, left, bottom)
 
-  newItem('assets/sword.png', '500px', '405px');
-  newItem('assets/shield.png', '165px', '185px');
-  newItem('assets/staff.png', '600px', '100px');
+  object.addEventListener('dblclick', () => {
+      object.remove()
+  })
+}
 
-  }
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
